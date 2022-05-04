55b80f82680f7d31f7901f17a410763223f57216
Brian <discountedaccess@gmail.com>
2022-05-04 10:40:56 +0200

    Initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { routes } from "./routes";
-
 const App = () => {
   return (
     <div className="App">
